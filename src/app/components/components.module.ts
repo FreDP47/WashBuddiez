a884d6370d57f15d255e2bd64108a10d32adc3ae
Asit <asit.vssut@gmail.com>
2019-03-25 23:31:58 +0530

    removed two unnecessary components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,14 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 
-import { BasicelementsComponent } from './basicelements/basicelements.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { TypographyComponent } from './typography/typography.component';
 import { NucleoiconsComponent } from './nucleoicons/nucleoicons.component';
 import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
-import { NgbdModalComponent } from './modal/modal.component';
-import { NgbdModalContent } from './modal/modal.component';
+
 
 
 @NgModule({
@@ -25,15 +23,12 @@ import { NgbdModalContent } from './modal/modal.component';
     ],
     declarations: [
         ComponentsComponent,
-        BasicelementsComponent,
         NavigationComponent,
         TypographyComponent,
         NucleoiconsComponent,
-        NotificationComponent,
-        NgbdModalComponent,
-        NgbdModalContent
+        NotificationComponent, 
     ],
-    entryComponents: [NgbdModalContent],
+    
     exports:[ ComponentsComponent ]
 })
 export class ComponentsModule { }
